dc77b85e5929d10ce09b57a62fc8f50f17cb0e3c
Devin Chen <d3vin.chen@gmail.com>
2017-08-31 11:02:20 +0800

    success to take a snapshot

--- baidu.js
+++ baidu.js
@@ -1,5 +1,10 @@
+/* eslint-disable max-len */
+
 var page = require('webpage').create();
-page.open('https://baidu.com/', function() {
+
+// page.viewportSize = { width: 412, height: 732 }; // Nexus 5X
+// page.settings.userAgent = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Mobile Safari/537.36';
+page.open('https://baidu.com/', function () {
   page.render('baidu.png');
   phantom.exit();
 });
--- snapshot.js
+++ snapshot.js
@@ -9,13 +9,19 @@ var reportUrl = 'https://acc.yonyoucloud.com/fireport/ReportServer?reportlet=%2F
 // reportUrl = 'https://euc.yonyoucloud.com/cas/login?sysid=yonyoufi&service=https%3A%2F%2Facc.yonyoucloud.com%2Ffireport%2FReportServer%3Freportlet%3D%252Fyonyoufi%252Foas6lr3w%252FMDupont%252FMDupont_p.cpt%26__showtoolbar__%3Dfalse%26aid%3Dyonyoufi%26tid%3Doas6lr3w%26userid%3D521fa89e-886e-4d44-9014-eb53097872d1%26referAppId%3Dyonyoufi%26tenantId%3Doas6lr3w%26op%3Dh5%26accbook%3D1574FCFD-89C8-4DFD-9E22-B281222D8C72';
 // reportUrl = 'https://baidu.com';
 // reportUrl = 'https://acc.yonyoucloud.com';
-reportUrl = 'https://acc.yonyoucloud.com/m/shouye';
+// reportUrl = 'https://acc.yonyoucloud.com/m/shouye';
 
-console.log('INSIDE SCRIPT!');
-console.log(' - phantom.scriptName: ' + JSON.stringify(phantom.scriptName));
-console.log(' - phantom.libraryPath: ' + JSON.stringify(phantom.libraryPath));
-console.log(' - phantom.args: ' + JSON.stringify(phantom.args));
-console.log(' - system.args: ' + JSON.stringify(system.args));
+function log() {
+  if (console) {
+    console.log.apply(console, arguments);
+  }
+}
+
+log('INSIDE SCRIPT!');
+log(' - phantom.scriptName: ' + JSON.stringify(phantom.scriptName));
+log(' - phantom.libraryPath: ' + JSON.stringify(phantom.libraryPath));
+log(' - phantom.args: ' + JSON.stringify(phantom.args));
+log(' - system.args: ' + JSON.stringify(system.args));
 
 // page setting
 
@@ -24,6 +30,7 @@ console.log(' - system.args: ' + JSON.stringify(system.args));
 // eslint-disable-next-line max-len
 page.settings.userAgent = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Mobile Safari/537.36';
 // page.settings.javascriptEnabled = false;
+
 // page.viewportSize = { width: 1920, height: 1080 };
 // page.viewportSize = { width: 372, height: 300 }; // iframe
 page.viewportSize = { width: 412, height: 732 }; // Nexus 5X
@@ -37,7 +44,7 @@ phantom.onError = function (msg, trace) {
       msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
     });
   }
-  console.log('\n' + msgStack.join('\n') + '\n');
+  log('\n' + msgStack.join('\n') + '\n');
   phantom.exit(1);
 };
 
@@ -55,72 +62,94 @@ page.onError = function (msg, trace) {
 };
 
 page.onConsoleMessage = function (msg, lineNum, sourceId) {
-  console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
+  log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
 };
 
 // page loading
 
 // page.onResourceReceived = function (response) {
-//   console.log('[onResourceReceived] response', response);
-//   console.log('[onResourceReceived] Response (#'
+//   log('[onResourceReceived] response', response);
+//   log('[onResourceReceived] Response (#'
 //     + response.id + ', stage "' + response.stage + '"): '
 //     + JSON.stringify(response));
 // };
 
 page.onNavigationRequested = function (url, type, willNavigate, main) {
-  console.log('[onNavigationRequested] Trying to navigate to: ' + url);
-  console.log('[onNavigationRequested] Caused by: ' + type);
-  console.log('[onNavigationRequested] Will actually navigate: ' + willNavigate);
-  console.log('[onNavigationRequested] Sent from the page\'s main frame: ' + main);
+  log('[onNavigationRequested] Trying to navigate to: ' + url);
+  log('[onNavigationRequested] Caused by: ' + type);
+  log('[onNavigationRequested] Will actually navigate: ' + willNavigate);
+  log('[onNavigationRequested] Sent from the page\'s main frame: ' + main);
 };
 
 page.onResourceTimeout = function (request) {
-  console.log('[onResourceTimeout] Response (#' + request.id + '): ' + JSON.stringify(request));
+  log('[onResourceTimeout] Response (#' + request.id + '): ' + JSON.stringify(request));
 };
 
 page.onUrlChanged = function (targetUrl) {
-  console.log('[onUrlChanged] New URL:', targetUrl);
+  log('[onUrlChanged] New URL:', targetUrl);
 };
 
 // page.onResourceRequested = function (requestData, networkRequest) {
-//   console.log('[onResourceRequested] Request (#' + requestData.id + '): '
+//   log('[onResourceRequested] Request (#' + requestData.id + '): '
 //     + JSON.stringify(requestData));
-//   console.log('[onResourceRequested] networkRequest:', networkRequest);
+//   log('[onResourceRequested] networkRequest:', networkRequest);
 // };
 
 // app
 
 if (!phantom.injectJs('passwd.js')) {
-  console.log('failed to load passwd.js');
+  log('failed to load passwd.js');
   phantom.exit();
 }
 
-console.log('username and password:', passwd.username, passwd.password);
+log('load username and password from config file:', passwd.username, passwd.password);
 
-function renderPage(url) {
-  console.log('page.open()', url);
-  page.open(url, function (status) {
-    console.log('[open] status', status);
-    if (status !== 'success') {
-      console.log('[open] status is not success:', status);
-      phantom.exit();
+function login() {
+  page.evaluate(function (config) {
+    function log() { // eslint-disable-line no-shadow
+      if (console) console.log.apply(console, arguments);
     }
-    page.evaluate(function (config) {
-      console.log('username and password:', config.username, config.password);
+    log('get username and password from phantom scope:', config.username, config.password);
+    if (document.querySelector('#fm1') === null) {
+      log('login form not exist');
+    } else {
+      log('found login form element');
+      log('try to input username and password');
       document.querySelector("input[name='username']").value = config.username;
       document.querySelector("input[type='password']").value = config.password;
       // document.querySelector('#fm1').submit();
+      log('do login');
       window.doLogin();
-    }, passwd);
+    }
+  }, passwd);
+}
 
-    console.log('wait 5s to take snapshot');
-    setTimeout(function () {
-      console.log('page.render()');
-      page.render('snapshot.png');
-
-      console.log('phantom.exit()');
+function renderPage(url) {
+  log('page.open()', url);
+  page.open(url, function (status) {
+    log('[open] status', status);
+    if (status !== 'success') {
+      log('[open] status is not success:', status);
       phantom.exit();
-    }, 10000);
+    }
+
+    log('wait 10s to login');
+    setTimeout(function () {
+      login();
+
+      log('wait 20s to take snapshot');
+      setTimeout(function () {
+        log('get useragent');
+        page.evaluate(function () {
+          console.log(navigator.userAgent);
+        });
+        log('page.render()');
+        page.render('snapshot.png');
+
+        log('phantom.exit()');
+        phantom.exit();
+      }, 10000);
+    }, 20000);
   });
 }
 
